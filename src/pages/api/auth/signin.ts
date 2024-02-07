e1fa22512f53aad4472b204d0841ca257d6542fc
Gilbert Hawkins <gilbert.t.hawkins@gmail.com>
2024-02-07 09:48:32 -0500

    callback url fix

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -15,7 +15,7 @@ export const POST: APIRoute = async (context: APIContext) => {
 				scopes: "email profile https://graph.microsoft.com/Mail.ReadWrite https://graph.microsoft.com/Mail.Send https://graph.microsoft.com/Calendars.ReadWrite https://graph.microsoft.com/User.Read",
 				redirectTo: import.meta.env.DEV
 					? "http://localhost:4321/api/auth/callback"
-					: "https://min-astro-auth.vercel.app/api/auth/callback",
+					: "https://astro-supabase-ssr-oauth.app/api/auth/callback",
 			},
 		});
 
