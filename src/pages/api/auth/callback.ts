449e01a9232e9461cdba906e47a721790025319f
Gilbert Hawkins <gilbert.t.hawkins@gmail.com>
2024-02-15 08:06:59 -0500

    minor edit

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -1,10 +1,9 @@
-import { createClient } from "src/lib/server";
+import { createClient } from "@lib/server";
 import { type APIContext, type APIRoute } from "astro";
 
 export const GET: APIRoute = async (context: APIContext) => {
 	const requestUrl = new URL(context.request.url);
 	const code = requestUrl.searchParams.get("code");
-
 	if (code) {
 		const supabase = createClient(context);
 
