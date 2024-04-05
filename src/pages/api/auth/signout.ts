ca03cc4517faa8e5998d1ff91320ca3dfe6c0938
Gilbert Hawkins <gilbert.t.hawkins@gmail.com>
2024-04-05 10:28:33 -0400

    fix delete cookies on signout

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -1,7 +1,9 @@
 import type { APIContext, APIRoute } from "astro";
 import { createClient } from "@lib/server";
 export const GET: APIRoute = async (context: APIContext) => {
-	const supabase = createClient(context);
-	await supabase.auth.signOut();
-	return context.redirect("/");
+  const supabase = createClient(context);
+  context.cookies.delete("provider_token", { path: "/" });
+  context.cookies.delete("provider_refresh_token", { path: "/" });
+  await supabase.auth.signOut();
+  return context.redirect("/");
 };
