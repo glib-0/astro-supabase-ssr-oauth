3d02078d4a87f1f0f782887b5f3e6ac00774c459
Gilbert Hawkins <gilbert.t.hawkins@gmail.com>
2024-04-09 12:12:37 -0400

    update  for minimal example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   integrations: [tailwind()],
   adapter: vercel(),
-  trailingSlash:'ignore',
-
-  redirects:{ }
+  trailingSlash: "ignore",
 });
--- src/lib/server.ts
+++ src/lib/server.ts
@@ -2,22 +2,22 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import type { APIContext } from "astro";
 
 export const createClient = (context: APIContext) => {
-	const supabase = createServerClient(
-		import.meta.env.PUBLIC_SUPABASE_URL,
-		import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
-		{
-			cookies: {
-				get(key: string) {
-					return context.cookies.get(key)?.value;
-				},
-				set(key: string, value: string, options: CookieOptions) {
-					context.cookies.set(key, value, options);
-				},
-				remove(key: string, options) {
-					context.cookies.delete(key, options);
-				},
-			},
-		}
-	);
-	return supabase;
+  const supabase = createServerClient(
+    import.meta.env.PUBLIC_SUPABASE_URL,
+    import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get(key: string) {
+          return context.cookies.get(key)?.value;
+        },
+        set(key: string, value: string, options: CookieOptions) {
+          context.cookies.set(key, value, options);
+        },
+        remove(key: string, options) {
+          context.cookies.delete(key, options);
+        },
+      },
+    }
+  );
+  return supabase;
 };
